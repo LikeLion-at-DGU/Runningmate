25fbf597e3568cb65e47d6a4c5cdc411a068273b
yaena1223 <yaena1223@dgu.ac.kr>
2022-06-23 20:43:14 +0900

    0623 캘린더페이지 기본셋팅

--- runningmate/mateapp/views.py
+++ runningmate/mateapp/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def showmain(request):
     return render(request,'mateapp/mainpage.html')
+
+def calendar(request):
+    return render(request,'mateapp/calendar.html')
