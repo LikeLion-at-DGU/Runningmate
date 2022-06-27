1a8800e61aac300ea85e2fe992b78e628118e8ea
yaena1223 <yaena1223@dgu.ac.kr>
2022-06-28 02:47:29 +0900

    0628 로그인 페이지 구현(진행중)

--- runningmate/mateapp/views.py
+++ runningmate/mateapp/views.py
@@ -7,15 +7,8 @@ def showmain(request):
     return render(request,'mateapp/mainpage.html')
 
 def login(request):
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            auth.login(request, user)
-            return redirect('mateapp/showmain')
-        else:
-            return render(request, 'mateapp/login.html', {'error': 'username or password is incorrect.'})
+    if request.user.is_authenticated:
+        return render(request,'mateapp/mainpage.html')
     else:
         return render(request, 'mateapp/login.html')
 
