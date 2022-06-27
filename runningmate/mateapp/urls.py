daf41c93b8ea44604856e573060cb91dc6b4dff4
yaena1223 <yaena1223@dgu.ac.kr>
2022-06-28 02:35:38 +0900

    0628 로그인 페이지 구현

--- runningmate/mateapp/urls.py
+++ runningmate/mateapp/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import *
 app_name = "mateapp"
 urlpatterns = [
-    path('',showmain, name = "showmain"),
+    path('mainpage',showmain, name = "showmain"),
     path('calendar',calendar, name = "calendar"),
+    path('', login, name="login"),
 ]
--- runningmate/mateapp/views.py
+++ runningmate/mateapp/views.py
@@ -1,11 +1,23 @@
-from django.shortcuts import render
-
+from django.contrib import auth
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
 # Create your views here.
 def showmain(request):
     return render(request,'mateapp/mainpage.html')
 
 def login(request):
-    return render(request, 'mateapp/login.html')
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('mateapp/showmain')
+        else:
+            return render(request, 'mateapp/login.html', {'error': 'username or password is incorrect.'})
+    else:
+        return render(request, 'mateapp/login.html')
 
 def calendar(request):
     return render(request,'mateapp/calendar.html')
