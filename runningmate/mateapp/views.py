2945ba5e115c1202e1a609cf915a8f7ef92449b6
Seulgigi <slki1205@naver.com>
2022-07-07 22:16:13 +0900

    mainpage 오류 수정

--- runningmate/mateapp/views.py
+++ runningmate/mateapp/views.py
@@ -12,7 +12,7 @@ sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 from addproject import models
 
 def showmain(request):
-    calendar = Calendar.objects.filter(writer=request.user, datetime__contains=datetime.date.today(
+    calendar = Calendar.objects.filter(writer=request.user, endday__contains=datetime.date.today(
     )).order_by('endday')  # 글을 작성한 유저의 캘린더 정보만 가져오겠다. 가까운 날짜 순으로 정렬
     project = Project.objects.all()
     return render(request, 'mateapp/mainpage.html', {'calendar': calendar, 'project':project})
