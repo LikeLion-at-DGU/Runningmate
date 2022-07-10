bc7443bbea3e57e65c514a95313d532fca0f31d2
AsherOh <wnstj701@naver.com>
2022-07-10 17:43:03 +0900

    투데이 오늘 날짜 요일 표시

--- runningmate/runningmate/static/js/today.js
+++ runningmate/runningmate/static/js/today.js
@@ -1,18 +1,36 @@
-let today = new Date.now();
+// let today = new Date.now();
 
+// var week = [
+//     "Sun",
+//     "Mon",
+//     "Tue",
+//     "Wed",
+//     "Thu",
+//     "Fri",
+//     "Sat",
+// ];
+// const renderDay = () => {
+//     const viewDay = date.getDay();
+//     const weekofday = week[viewDay];
+
+//     document.querySelector(".today_day").textContent = '{weekofday}';
+// } // today_day
+
+
+const today1 = new Date();
+
+const year = today1.getFullYear();
+const month = today1.getMonth() + 1;
+const day = today1.getDate();
 var week = [
-    "Sun",
-    "Mon",
-    "Tue",
-    "Wed",
-    "Thu",
-    "Fri",
-    "Sat",
+    "SUNDAY",
+    "MONDAY",
+    "TUESDAY",
+    "WEDENSDAY",
+    "THURSDAY",
+    "FRIDAY",
+    "SATURDAY",
 ];
-const renderDay = () => {
-    const viewDay = date.getDay();
-    const weekofday = week[viewDay];
 
-    document.querySelector(".today_day").textContent = '{weekofday}';
-} // today_day
-
+document.querySelector(".day_week").innerHTML = week[today1.getDay()];
+document.querySelector(".date_week").innerHTML = year + '.' + month + '.' + day;
